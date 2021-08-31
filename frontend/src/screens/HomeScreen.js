8c86bc450da4411d7ff8a1a60a0ba5f6de913b4c
Keshava Varma <keshavavarma.ns@gmail.com>
2021-08-31 17:01:40 +0530

    Dynamic Product-list rendering

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,29 @@
+import Data from "../Data.js";
+const HomeScreen = {
+  render: () => {
+    const { products } = Data;
+    console.log(products.map((product) => product));
+    return `
+        <ul class='products'>
+        ${products
+          .map(
+            (product) => `
+        <li>
+            <div class="product">
+              <div class="product-image">
+                <a href="/#/product/${product._id}">
+                  <img src="${product.image}" alt="product-1"
+                /></a>
+              </div>
+              <div class="product-title">
+                <a href="/#/product/${product._id}">${product.name}</a>
+              </div>
+              <div class="product-brand">${product.brand}</div>
+              <div class="product-price">$${product.price}</div>
+            </div>
+        </li>`
+          )
+          .join("\n")}</ul>`;
+  },
+};
+export default HomeScreen;
