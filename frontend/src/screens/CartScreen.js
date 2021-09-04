d0929387c8d1de5260ab8dac0b87cd8945167a97
Keshava Varma <keshavavarma.ns@gmail.com>
2021-09-04 12:11:12 +0530

    style: cart screen styling

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -31,8 +31,76 @@ const CartScreen = {
         qty: 1,
       });
     }
-    return `<div>This is Cart Screen</div>
-    <div>${getCartItems().length}</div>`;
+    const cartItems = getCartItems();
+
+    return `
+        ${
+          cartItems.length === 0
+            ? `<div>No Items in Cart</div>`
+            : `<div class="cart">
+        <div class="cart-items">
+            <ul class='cart-list'>
+                <li>
+                    <h3>Shopping Cart</h3>
+                    <h3>Price</h3>
+                </li>
+                ${cartItems
+                  .map(
+                    (item) => `
+                <li class='cart-item-container'>
+                    <div class='cart-item'>
+                        <div class="product-image-sm">
+                            <img src="${item.image}" alt="${item.name}">
+                        </div> 
+                        <div class ="item-detail">
+                            <p>${item.name}</p>
+                            <div>
+                                <span>Qty :</span>
+                                <select name="qty" id="qty" class='qty'>
+                                    ${[...Array(item.count).keys()].map((x) =>
+                                      item.qty === x + 1
+                                        ? `<option selected value='${x + 1}'>${
+                                            x + 1
+                                          }</option>`
+                                        : `<option  value='${x + 1}'>${
+                                            x + 1
+                                          }</option>`
+                                    )}
+                                </select>
+                                <button class='delete-btn'><i class="fas fa-trash fa-lg" ></i></button>
+                            </div>
+                        </div>
+                    </div> 
+                    <div class='cart-item-price'>
+                        $${item.price}
+                    </div>
+                 </li>
+                `
+                  )
+                  .join("\n")}                
+            </ul>
+        </div>
+        <div class="subtotal-container">
+            <div class="subtotal">
+                <h2>Subtotal (${
+                  (cartItems.reduce((a, c) => {
+                    return (a += c.qty);
+                  }),
+                  0)
+                }) : $${
+                (cartItems.reduce((a, c) => {
+                  return (a += c.price * c.qty);
+                }),
+                0)
+              }
+                </h2>
+                <button class='proceed-to-cart'>Proceed to Checkout</button>
+            </div>
+        </div>
+    </div>`
+        }
+        
+    `;
   },
 };
 
