071c7462d71986f20908cbed4e59e316becb6e33
Keshava Varma <keshavavarma.ns@gmail.com>
2021-09-06 19:05:10 +0530

    jwt authentication backend

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,19 +1,46 @@
 const express = require("express");
 const User = require("../models/user.js");
-
+const expressAsyncHandler = require("express-async-handler");
+const generateToken = require("../util.js");
 const userRouter = express.Router();
 
-module.exports = userRouter.get("/createAdmin", async (req, res) => {
-  try {
-    const user = new User({
-      name: "admin",
-      email: "admin@example.com",
-      password: "admin",
-      isAdmin: true,
+userRouter.get(
+  "/createAdmin",
+  expressAsyncHandler(async (req, res) => {
+    try {
+      const user = new User({
+        name: "admin",
+        email: "admin@example.com",
+        password: "admin",
+        isAdmin: true,
+      });
+      const createdUser = await user.save();
+      res.send(createdUser);
+    } catch (error) {
+      res.status(500).send({ message: error.message });
+    }
+  })
+);
+
+userRouter.post(
+  "/signin",
+  expressAsyncHandler(async (req, res) => {
+    const signInUser = await User.findOne({
+      email: req.body.email,
+      password: req.body.password,
     });
-    const createdUser = await user.save();
-    res.send(createdUser);
-  } catch (error) {
-    res.status(500).send({ message: error.message });
-  }
-});
+    if (!signInUser) {
+      res.status(401).send({ message: "Invalid Email or Password" });
+    } else {
+      res.send({
+        _id: signInUser._id,
+        name: signInUser.name,
+        email: signInUser.email,
+        isAdmin: signInUser.isAdmin,
+        token: generateToken(signInUser),
+      });
+    }
+  })
+);
+
+module.exports = userRouter;
--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ mongoose
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 app.use("/api/users", userRouter);
 app.get("/api/products", (req, res) => {
   res.send(Data.products);
@@ -27,6 +28,10 @@ app.get("/api/products/:id", (req, res) => {
   } else {
     res.status(404).send({ message: "Product Not found" });
   }
+});
+app.use((err, req, res, next) => {
+  const status = err.name && err.name === "ValidationError" ? 400 : 500;
+  res.status(status).send({ message: err.message });
 });
 app.listen(5500, () => {
   console.log("serve at http://localhost:5500");
