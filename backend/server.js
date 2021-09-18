ea21f24e0203de7acfc47ccf74fa14ef75c50aa9
Keshava Varma <keshavavarma.ns@gmail.com>
2021-09-18 18:17:37 +0530

    Prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const config = require("./config.js");
 const userRouter = require("./routes/userRouter.js");
 const orderRouter = require("./routes/orderRouter.js");
+const path = require("path");
 
 mongoose
   .connect(config.MONGODB_URL)
@@ -24,6 +25,10 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
+app.use((err, req, res, next) => {
+  const status = err.name && err.name === "ValidationError" ? 400 : 500;
+  res.status(status).send({ message: err.message });
+});
 app.get("/api/paypal/clientId", (req, res) => {
   res.send({ clientId: config.PAYPAL_CLIENT_ID });
 });
@@ -38,10 +43,20 @@ app.get("/api/products/:id", (req, res) => {
     res.status(404).send({ message: "Product Not found" });
   }
 });
-app.use((err, req, res, next) => {
-  const status = err.name && err.name === "ValidationError" ? 400 : 500;
-  res.status(status).send({ message: err.message });
-});
-app.listen(5500, () => {
+
+// serve static assets in production
+
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("frontend/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5500;
+
+app.listen(PORT, () => {
   console.log("serve at http://localhost:5500");
 });
